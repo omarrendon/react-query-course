35eb2ca1563df8832874f6231a05f86b3464e20c
Omar Rendon <omarcr.96@gmail.com>
2022-11-04 14:52:30 -0600

    fecthing random number without react-query

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
-import logo from './logo.svg';
+import { useEffect, useReducer, useState } from 'react';
+
 import './App.css';
 
+const getRandomNumberFromAPI = async () => {
+  const response = await fetch('https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new');
+  const numberString = await response.text();
+  console.log({ numberString });
+
+  return +numberString;
+};
+
 function App() {
+  const [number, setNumber] = useState(0);
+  const [isLoading, setisLoading] = useState(true);
+  const [error, setError] = useState('');
+  const [key, forceRefecth] = useReducer((x) => x + 1, 0);
+
+  useEffect(() => {
+    setisLoading(true);
+    getRandomNumberFromAPI()
+      .then(num => setNumber(num))
+      .catch(error => setError(error.message));
+  }, [key]);
+
+  useEffect(() => {
+    if (number) setisLoading(false);
+  }, [number]);
+
+  useEffect(() => {
+    if (error) setisLoading(false);
+  }, [error]);
+
   return (
-    <div className="App">
-    
+    <div className="App App-header">
+      {isLoading
+        ? (<h1>Cargando ...</h1>)
+        : (<h2>Número aleatorio : {number}</h2>)
+      }
+      {!isLoading && error && <h3>error</h3>}
+
+      <button
+        onClick={forceRefecth}
+      >
+        {isLoading ? '.....' : 'Refecth'}
+      </button>
     </div>
   );
 }
