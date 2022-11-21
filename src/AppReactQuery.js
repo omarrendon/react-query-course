d1307c920c888c37b99584ee6dd94769e7870c71
Omar Rendon <omarcr.96@gmail.com>
2022-11-21 15:04:57 -0600

    ADD: fetching data with axios and react-query

--- src/AppReactQuery.js
+++ src/AppReactQuery.js
@@ -1,21 +1,32 @@
 import './App.css';
 import { useRandom } from './hooks/useRandom';
 
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+
+
+// Create a client
+const queryClient = new QueryClient();
+
 export default function AppReactQuery() {
   const query = useRandom();
 
   return (
-    <div className="App App-header">
-      {query.isFetching
-        ? (<h1>Cargando ...</h1>)
-        : (<h2>Número aleatorio : {query.data}</h2>)
-      }
-      {!query.isLoading && query.isError && <h3>{query.error}</h3>}
-      <button
-        onClick={() => query.refetch()}
-      >
-        {query.isFetching ? '.....' : 'Refecth'}
-      </button>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App App-header">
+        {query.isFetching
+          ? (<h1>Cargando ...</h1>)
+          : (<h2>Número aleatorio : {query.data}</h2>)
+        }
+        {!query.isLoading && query.isError && <h3>{query.error}</h3>}
+        <button
+          onClick={() => query.refetch()}
+        >
+          {query.isFetching ? '.....' : 'Refecth'}
+        </button>
+      </div>
+    </QueryClientProvider>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,31 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 
 import App from './App';
 import AppReactQuery from './AppReactQuery';
-
-import reportWebVitals from './reportWebVitals';
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query';
-
-import { RouterProvider } from 'react-router';
-import { router } from './router';
-
-
 import Main from './Main';
 
-// Create a client
-const queryClient = new QueryClient()
+import reportWebVitals from './reportWebVitals';
+import './index.css';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <QueryClientProvider client={queryClient}>
   //   {/* <App /> */}
   //   <AppReactQuery />
-  // </QueryClientProvider>
   <Main />
 );
 
