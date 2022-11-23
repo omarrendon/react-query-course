c0b91b8c43efcc098cbcbb671f9a276025f207e6
Omar Rendon <omarcr.96@gmail.com>
2022-11-23 13:35:22 -0600

    ADD: select labels and show labels from the data of the request

--- src/hooks/useLabels.js
+++ src/hooks/useLabels.js
@@ -14,10 +14,20 @@ export default function useLabels() {
     ['labels'],
     getLabels,
     // false is equal to avoid refecth data when the user on focus the window each timess
-    // staleTimne: is to set the time to refetch the petition  
+    // staleTimne: is to set the time to refetch the petition
+    // placeHolderData: is to show data meanwhile make the request to the server
     {
       refetchOnWindowFocus: false,
-      staleTime: 1000 * 60 * 60,
+      // staleTime: 1000 * 60 * 60,
+      placeholderData: [{
+        id: 791921801,
+        node_id: "MDU6TGFiZWw3OTE5MjE4MDE=",
+        url: "https://api.github.com/repos/facebook/react/labels/%E2%9D%A4%EF%B8%8F",
+        name: "❤️",
+        color: "ffffff",
+        default: false,
+        description: null,
+      }]
     }
   );
   return labelsQuery;
--- src/issues/components/LabelPicker.js
+++ src/issues/components/LabelPicker.js
@@ -2,22 +2,23 @@ import useLabels from "../../hooks/useLabels"
 import LoadingIcon from "../../shared/components/LoadingIcon";
 
 
-export const LabelPicker = () => {
+export const LabelPicker = ({ selectedLabels, onChange }) => {
   const labels = useLabels();
 
   if (labels.isLoading)
     return (<LoadingIcon />)
 
-  console.log({ labels });
+  // console.log({ labels });
   return (
     <>
       {labels.data?.map((label) => (
         <span
           key={label.id}
-          className="badge rounded-pill m-1 label-picker"
+          className={`badge rounded-pill m-1 label-picker ${selectedLabels.includes(label.name) ? 'label-active' : ''}`}
           style={{
             border: `1px solid #${label.color}`, color: `#${label.color}`
           }}
+          onClick={() => onChange(label.name)}
         >
           {label.name}
         </span>
