aae86e8bed2589c1d0bce78b3523460e7086e422
Omar Rendon <omarcr.96@gmail.com>
2022-11-24 10:51:20 -0600

    Create useIssue to get a specific issue from githubAPI

---
+++ src/hooks/useIssue.js
@@ -0,0 +1,24 @@
+
+import { useQuery } from '@tanstack/react-query';
+import { githubApi } from '../api/githubApi';
+
+const getIssueInfo = async (issueNumber) => {
+  const { data } = await githubApi.get(`/issues/${issueNumber}`,
+    {
+      headers: {
+        Authorization: null
+      }
+    });
+  return data;
+};
+
+export default function useIssue(issueNumber) {
+  const issueQuery = useQuery(
+    ['Issue', issueNumber],
+    () => getIssueInfo(issueNumber )
+  );
+
+  return {
+    issueQuery
+  };
+}
